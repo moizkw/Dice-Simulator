03101d6061e5960490245dd51293ade70b6528f8
moizkw <86135505+moizkw@users.noreply.github.com>
2021-11-07 15:48:14 -0500

    Update a1.py

--- a1.py
+++ a1.py
@@ -1,27 +1,9 @@
 # Author: Moiz Kharodawala, 500968133
-# CPS109-06
-# Assignment 1
-
-# ~22 lines of comments
-# ~45 lines of code
-# Problem and Solution are described below
-# All 5 learning objects completed (shown below after each)
-# The code successfully solves the program
-#          Check output.txt and the detailed explanations below 
-# The code is my original work.
-# So please give me a 100...thanks!
-
-# Problem: Engineers study a lot and we really need a Ludo break from all the carnage. 
-#          Unfortunately, I have the board of the game but I lost the dice.
-
-# Solution: A dice simulator that solves the problem of having no physical dice. 
-#           The code can be found below:
 
 # Necessary Import
 import random
 
 # Required Function
-# Learning Objective 5 completed
 def moiz_dice_simulator():    
     
     #Initializing roll_it as true in the beginning so the dice can roll atleast once.
@@ -29,15 +11,13 @@ def moiz_dice_simulator():
 
     # While loop that makes sure that the dice rolls every time the user wishes
     # Used boolean primitive data type
-    # Learning Objectives 2 & 4 completed
     while roll_it == True:
 
         # Generating a random digit between 1-6, which a dice is supposed to do
         # This is where we use our import
         result = random.randint(1,6) 
 
-    # Implementing decisions using if statements
-    # Learning Objective 1 completed        
+    # Implementing decisions using if statements      
         # First Condition
         if result == 1:
             print("-----------")
@@ -85,11 +65,11 @@ def moiz_dice_simulator():
         roll_again = input("Enter 'y' to roll again or press any other key to exit: ") 
         
         # Comparing strings- 'roll_again' with 'y'
-        # Learning Objective 3 completed
         if roll_again != 'y':
             # Continung the loop in case the user enters 'y'
             # And breaking the loop otherwise
-            roll_it = False        
+            roll_it = False  
+    #End of while loop
          
 # Main Funtion
 if __name__ == "__main__":
